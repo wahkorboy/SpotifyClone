eec43940f02338a1e82e8a5af3b4ed7e021b543b
laptop <namboonma@gmail.com>
2021-08-09 13:52:56 +0700

    Part 8

--- app/src/main/java/com/wahkor/spotifyclone/exo/MusicService.kt
+++ app/src/main/java/com/wahkor/spotifyclone/exo/MusicService.kt
@@ -4,11 +4,14 @@ import android.app.PendingIntent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.wahkor.spotifyclone.exo.callbacks.MusicPlaybackPrepare
+import com.wahkor.spotifyclone.exo.callbacks.MusicPlayerEventListener
 import com.wahkor.spotifyclone.exo.callbacks.MusicPlayerNotificationListener
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -16,48 +19,81 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import javax.inject.Inject
-private const val SERVICE_TAG="Spotify Clone Service"
+
+private const val SERVICE_TAG = "Spotify Clone Service"
+
 @AndroidEntryPoint
-class MusicService:MediaBrowserServiceCompat() {
+class MusicService : MediaBrowserServiceCompat() {
 
-    @Inject lateinit var dataSourceFactory: DefaultDataSourceFactory
-    @Inject lateinit var exoPlayer:SimpleExoPlayer
+    @Inject
+    lateinit var dataSourceFactory: DefaultDataSourceFactory
+    @Inject
+    lateinit var exoPlayer: SimpleExoPlayer
+    @Inject
+    lateinit var firebaseMusicSource: FirebaseMusicSource
 
-    private val serviceJob= Job()
-    private val serviceScope= CoroutineScope(Dispatchers.Main+serviceJob)
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
-    private lateinit var mediaSession:MediaSessionCompat
-    private lateinit var mediaSessionConnector:MediaSessionConnector
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
     var isForegroundService = false
     private lateinit var musicNotificationManager: MusicNotificationManager
+    private var curPlaySong: MediaMetadataCompat? = null
 
     override fun onCreate() {
         super.onCreate()
-        val activityIntent=packageManager?.getLaunchIntentForPackage(packageName)?.let {
-            PendingIntent.getActivity(this,0,it,0)
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
         }
 
-        mediaSession=MediaSessionCompat(this, SERVICE_TAG).apply {
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
             setSessionActivity(activityIntent)
-            isActive=true
+            isActive = true
         }
-        sessionToken=mediaSession.sessionToken
+        sessionToken = mediaSession.sessionToken
         musicNotificationManager = MusicNotificationManager(
             this,
             mediaSession.sessionToken,
             MusicPlayerNotificationListener(this)
-        ){
+        ) {
 
         }
-        mediaSessionConnector= MediaSessionConnector(mediaSession)
+
+        val musicPlaybackPrepare = MusicPlaybackPrepare(firebaseMusicSource) {
+            curPlaySong = it
+            preparePlayer(
+                firebaseMusicSource.songs,
+                it,
+                true
+            )
+        }
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector.setPlaybackPreparer(musicPlaybackPrepare)
         mediaSessionConnector.setPlayer(exoPlayer)
 
+        exoPlayer.addListener(MusicPlayerEventListener(this))
+        musicNotificationManager.showNotification(exoPlayer)
+
+    }
+
+    private fun preparePlayer(
+        songs: List<MediaMetadataCompat>,
+        itemToPlay: MediaMetadataCompat?,
+        playNow: Boolean
+    ) {
+
+        val curSongIndex = if (curPlaySong == null) 0 else songs.indexOf(itemToPlay)
+        exoPlayer.prepare(firebaseMusicSource.asMediaSource(dataSourceFactory))
+        exoPlayer.seekTo(curSongIndex, 0L)
+        exoPlayer.playWhenReady = playNow
     }
 
     override fun onDestroy() {
         super.onDestroy()
         serviceScope.cancel()
     }
+
     override fun onGetRoot(
         clientPackageName: String,
         clientUid: Int,
---
+++ app/src/main/java/com/wahkor/spotifyclone/exo/callbacks/MusicPlayerEventListener.kt
@@ -0,0 +1,22 @@
+package com.wahkor.spotifyclone.exo.callbacks
+
+import android.widget.Toast
+import com.google.android.exoplayer2.ExoPlaybackException
+import com.google.android.exoplayer2.Player
+import com.wahkor.spotifyclone.exo.MusicService
+
+class MusicPlayerEventListener(
+    private val musicService: MusicService
+) : Player.EventListener{
+    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+        super.onPlayerStateChanged(playWhenReady, playbackState)
+        if (playbackState==Player.STATE_READY && !playWhenReady){
+            musicService.stopForeground(false)
+        }
+    }
+
+    override fun onPlayerError(error: ExoPlaybackException) {
+        super.onPlayerError(error)
+        Toast.makeText(musicService,"An unknown error occurred",Toast.LENGTH_SHORT).show()
+    }
+}
