17637035ed647fdbfabc16e8434d5b4d6be0c89d
fujitsu <namboonma@gmail.com>
2021-08-10 08:42:33 +0700

    Toggling the Play State - MVVM Spotify Clone - Part 16

--- app/src/main/java/com/wahkor/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/wahkor/spotifyclone/ui/MainActivity.kt
@@ -3,14 +3,18 @@ package com.wahkor.spotifyclone.ui
 import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
+import com.google.android.material.snackbar.Snackbar
 import com.wahkor.spotifyclone.R
 import com.wahkor.spotifyclone.adapter.SwipeSongAdapter
 import com.wahkor.spotifyclone.data.entities.Song
+import com.wahkor.spotifyclone.exo.isPlaying
 import com.wahkor.spotifyclone.exo.toSong
 import com.wahkor.spotifyclone.ui.viewmodels.MainViewModel
 import com.wahkor.spotifyclone.utils.Query
@@ -26,11 +30,9 @@ class MainActivity : AppCompatActivity() {
     @Inject lateinit var glide:RequestManager
 
     private var curPlayingSong: Song?=null
+    private var playbackState:PlaybackStateCompat?=null
     private val requestPermission =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
-            if (!granted) {
-            }
-
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +41,21 @@ class MainActivity : AppCompatActivity() {
         requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
         subscribeToObservers()
         vpSong.adapter=swipeSongAdapter
+        vpSong.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if(playbackState?.isPlaying==true){
+                    mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                }else{
+                    curPlayingSong=swipeSongAdapter.songs[position]
+                }
+            }
+        })
+        ivPlayPause.setOnClickListener {
+            curPlayingSong?.let { song ->
+                mainViewModel.playOrToggleSong(song,true)
+            }
+        }
 
     }
 
@@ -75,6 +92,46 @@ class MainActivity : AppCompatActivity() {
                 curPlayingSong=it.toSong()
                 glide.load(curPlayingSong?.imageUrl).into(ivCurSongImage)
                 switchViewPagerToCurrentSong(curPlayingSong?:return@observe)
+            }
+        }
+        mainViewModel.playbackState.observe(this){
+            playbackState=it
+            ivPlayPause.setImageResource(
+                if (playbackState?.isPlaying == true) R.drawable.exo_icon_pause
+            else R.drawable.exo_icon_play
+            )
+
+        }
+        mainViewModel.isConnected.observe(this){
+            it?.getContentIfNotHandled()?.let { result ->
+                run {
+                    when (result.status) {
+                        Status.Error -> {
+                            Snackbar.make(
+                                rootLayout,
+                                result.message ?: "An unknown error occurred",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+        mainViewModel.networkError.observe(this){
+            it?.getContentIfNotHandled()?.let { result ->
+                run {
+                    when (result.status) {
+                        Status.Error -> {
+                            Snackbar.make(
+                                rootLayout,
+                                result.message ?: "An unknown error occurred",
+                                Snackbar.LENGTH_LONG
+                            ).show()
+                        }
+                        else -> Unit
+                    }
+                }
             }
         }
 
